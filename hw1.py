f32394a1c4f2bafbdd789e5edfef42ffef0ddbc8
leo8883 <48037911+leo8883@users.noreply.github.com>
2019-04-10 15:10:47 +0800

    update hw

--- hw1.py
+++
@@ -1,5 +0,0 @@
-import requests
-response = requests.get("http://www.novelscape.net/wxxs/j/jingyong/ffwz/001.htm")
-response.encoding="big5"
-book1 = response.text
-book1[:]
--- hw2.py
+++
@@ -1,4 +0,0 @@
-x = input("請輸入數字： ")
-y = list(set(x))
-y.sort()
-not list(x) == y
